a85108e41c7094ab15ebb3566c89d8ba16bd885b
gairik9 <42964943+gairik9@users.noreply.github.com>
2022-03-01 13:34:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function trigger() {
+    Swal.fire({
+        title: "<h1>&#9989</h1>",
+        html: "<b>Successfully Logged In</b>",
+        // confirmButtonText: "<u>Ok</u>",
+    });
+}
+
+function turningOn() {
+    Swal.fire({
+        title: "<h1>Turning camera on....</h1>",
+        html: "<b>&#9889;</b>"});
+       
+}
